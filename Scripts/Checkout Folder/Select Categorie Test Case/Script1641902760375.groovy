11d8e26794830990c98b9e0f76681b7a5160c35f
fatma <fatma.naeem93@gmail.com>
2022-01-16 01:24:52 +0200

    refactor Fill Shipping and navigate to next step and observe the checkout review

--- Scripts/Checkout Folder/Select Categorie Test Case/Script1641902760375.groovy
+++ Scripts/Checkout Folder/Select Categorie Test Case/Script1641902760375.groovy
@@ -16,6 +16,7 @@ import validations.FiltersValidations
 import validations.GeneralValidations
 import validations.HeaderValidations
 import validations.ProductValidations
+import validations.QuickOrderValidations
 // ----------- Navigate to https://www.cleanersupply.com/----------
 GeneralHelpers.initScenario();
 HeaderValidations.verifyCartItemsNumber(0)
@@ -31,18 +32,16 @@ GeneralHelpers.newPageIsOpened(GlobalVariable.computerAndRegisterPageUrl, Global
 GeneralValidations.verifyBreadcrump('Object Repository/CategoryPage/li_BreadcrumpOfComputerAndRegisterPAge', GlobalVariable.breadcrumpOfComputerAndRegister)
 GeneralValidations.verifyTitleOfHeading(GlobalVariable.headingTitleOFComputerAndRegisterPage)
 // ----------- From the manufacturer section, select the 'Casio' manufacturer and select 'SP1000' Model. -------
-//ManufacterNumber = FiltersActions.storeManufacturerNumber()
+//Casio
 ManufacterLinkUrl = 'https://www.cleanersupply.com/Tags-Forms/Computer-Register/?Manufacturer=Casio'
 filtersGroupsNumber = FiltersActions.storeFiltersGroupsNumber()
-//packagingProductsLinkUrl = FiltersActions.storePackingProductsLinkUrl()
 FiltersValidations.verifyManufacturerFilterContent()
 GeneralActions.clickOnElement('Object Repository/Filters/ManufacturerFilter/span_ManufacturerOptionSelect')
 FiltersHelpers.verifyHoverOnManufacturerDropdowns('Object Repository/Filters/ManufacturerFilter/a_CasioSelected')
 GeneralActions.clickOnElement('Object Repository/Filters/ManufacturerFilter/a_CasioSelected')
 FiltersValidations.verifyManufacturerFilterApplied(ManufacterLinkUrl, filtersGroupsNumber, 'Casio')
-//model
+//model SP1000
 ManufacterLinkUrl = 'https://www.cleanersupply.com/Tags-Forms/Computer-Register/?Model+%23=SP1000&Manufacturer=Casio'
-//packagingProductsLinkUrl = FiltersActions.storePackingProductsLinkUrl()
 GeneralActions.clickOnElement('Object Repository/Filters/ManufacturerFilter/span_ModelOption')
 FiltersHelpers.verifyHoverOnManufacturerDropdowns('Object Repository/Filters/ManufacturerFilter/a_sp1000')
 GeneralActions.clickOnElement('Object Repository/Filters/ManufacturerFilter/a_sp1000')
@@ -56,15 +55,14 @@ double productPrice = ProductHelpers.verifypriceAndListPriceAndVolumePrice(first
 //ProductValidations.verifyNumberOfAvailableColors(firstProductMap.get('availableColors'))
 ProductValidations.verifybreadcrumbIsVisible()
 ProductValidations.verifyProductSKU()
-// --------- edit quantity value to be 5 ---------
+// --------- edit quantity value to be 10 ---------
 double discountedPrice = ProductValidations.formatPrice(ProductActions.storeDiscountedPriceManufacturer())
 List<Double> productsPrices = new ArrayList()
 productsPrices.add(15.54)
 List<Integer> productsQuantities = new ArrayList()
 productsQuantities.add(10)
 ProductHelpers.verifyUpdateQuantityAndAddToCart(10, discountedPrice, 1, productsPrices, productsQuantities)
-
-//--click on add to cart button --
+//click on add to cart button
 //GeneralValidations.verifyClickOnAddToCartButton()
 // ----------- Navigate to the cart. ----------------
 CheckOutHelpers.navigateToCart(GlobalVariable.casioName,GlobalVariable.casioQuantity,GlobalVariable.casioPrice,GlobalVariable.casioSku)
@@ -72,29 +70,11 @@ CheckOutHelpers.navigateToCart(GlobalVariable.casioName,GlobalVariable.casioQuan
 CheckOutHelpers.proceedToCheckOut()
 // ----------- Select 'Checkut As Guest' and move to the next step. ---------------
 CheckOutHelpers.selectCheckoutAsGuest(GlobalVariable.casioName,GlobalVariable.casioQuantity,GlobalVariable.casioPrice,GlobalVariable.casioSku)
-// ----------- 
+// ----------- Fill Shipping and navigate to next step and observe the checkout review------
+//checkout page
 CheckOutHelpers.proceedToReviewOrderFinish()
 GeneralHelpers.newPageIsOpened('/checkout',"Checkout - Cleaner's Supply")
 GeneralValidations.verifyCurrentPageHeading('Object Repository/General/h1-pageHeading','CHECKOUT')
-String addressSection=WebUI.getText(findTestObject('Object Repository/Checkout/Review Checkout Info/address-section'))
-addressSection.contains(GlobalVariable.CompanyName)
-addressSection.contains(GlobalVariable.firstName)
-addressSection.contains(GlobalVariable.lastName)
-addressSection.contains(GlobalVariable.address1)
-addressSection.contains(GlobalVariable.address2)
-addressSection.contains(Integer.toString(GlobalVariable.zipCode))
-addressSection.contains(GlobalVariable.city)
-String phone=GlobalVariable.phone
-String x=phone.substring(0,3)+"-"+phone.substring(3,6)+"-"+phone.substring(6,10)
-addressSection.contains(x)
-addressSection.contains('United States')
-assert WebUI.getText(findTestObject('Object Repository/Checkout/Review Checkout Info/header-shipping-address')).equals('SHIPPING ADDRESS')
-assert WebUI.getText(findTestObject('Object Repository/Checkout/Review Checkout Info/header-payment-method')).equals('PAYMENT METHOD')
-String paymentSection=WebUI.getText(findTestObject('Object Repository/Checkout/Review Checkout Info/payment-section'))
-assert paymentSection.contains(GlobalVariable.cardName)
-String cardNumber=GlobalVariable.cardNumber
-
-assert paymentSection.contains("**** "+cardNumber.substring(cardNumber.length() - 4))
-assert paymentSection.contains('8/'+'2026'.substring('2026'.length() - 2))
-GeneralValidations.verifyInputValue (findTestObject('Object Repository/Checkout/PAYMENT METHOD PAYMENT METHOD PAYMENT METHOD PAYMENT METHOD Payment Method Section/po'), GlobalVariable.po)
-GeneralValidations.verifyInputValue (findTestObject('Object Repository/Checkout/PAYMENT METHOD PAYMENT METHOD PAYMENT METHOD PAYMENT METHOD Payment Method Section/comments'), GlobalVariable.comment)
+QuickOrderValidations.verifyReviewInfoInCheckout()
+// ----------- close browser --------------------
+WebUI.closeBrowser();
