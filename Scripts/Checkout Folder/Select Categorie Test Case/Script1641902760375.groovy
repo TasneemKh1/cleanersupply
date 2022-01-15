bac06e325ee2a825750f2fe44dc33d5b577e2412
fatma shawaf <fatma.naeem93@gmail.com>
2022-01-15 21:18:09 +0200

    fix cart validation

--- Scripts/Checkout Folder/Select Categorie Test Case/Script1641902760375.groovy
+++ Scripts/Checkout Folder/Select Categorie Test Case/Script1641902760375.groovy
@@ -1,33 +1,18 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-import helpers.GeneralHelpers
-import helpers.ProductHelpers
+
 import actions.FiltersActions
 import actions.GeneralActions
-import actions.ProductActions
 import actions.SelectCategoriesActions
+import helpers.CheckOutHelpers
+import helpers.FiltersHelpers
+import helpers.GeneralHelpers
+import internal.GlobalVariable
 import validations.FiltersValidations
 import validations.GeneralValidations
 import validations.HeaderValidations
-import validations.ProductValidations
-import helpers.CheckOutHelpers
-import helpers.FiltersHelpers
 // ----------- Navigate to https://www.cleanersupply.com/----------
 GeneralHelpers.initScenario();
 HeaderValidations.verifyCartItemsNumber(0)
